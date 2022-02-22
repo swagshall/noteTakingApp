de83f8a92bbf8206c9b5a84730fb8297331e0346
Shifra Wagshall <shifrawagshall@gmail.com>
2022-02-22 01:49:25 -0500

    Removed api get and post code,

--- server.js
+++ server.js
@@ -10,46 +10,20 @@ const writeFileSync = util.promisify(fs.writeFile);
 const app = express();
 const port = process.env.PORT || 8000;
 
+
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
-app.use(express.static("./public"));
+// app.use(express.static("./public"));
 
-//get /api/notes
-module.exports = function (app) {
-    app.get("/api/notes", function (req, res) {
+//Require routes file
+require('./routes/routes')(app);
 
-        (readFileSync('./db.json', 'utf8'))
-          .then(data => {
-            console.log(data, "data-two")
-            //notes=[].concat(JSON.parse(data));
-            res.json(JSON.parse(data))
-          })
-          .catch(err => console.log(err))
-    
-      });}
+// Setup listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
 
-//post /api/notes
-// app.post("/api/notes", function (req, res) {
-//     let note=req.body;
-//     (readFileSync('./db.json', 'utf8'))
-//     .then(data => {
-      
-//       const notes=[].concat(JSON.parse(data));
-//       note.id=notes.length+1;
-//       res.json(JSON.parse(data))
-// })
 
-app.post("/api/notes", (req, res) => {
-    // let note=req.body;
-    const notes=[].concat(JSON.parse(data));
-    const newNote = {
-      title: req.body.title,
-      text: req.body.text,
-      id=notes.length+1,
-    };
-    writeFileSync(newNote).then(data => 
-        res.json(data)).catch((err) => 
-        console.log(err));
-  });
